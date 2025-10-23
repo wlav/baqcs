a18f01ea889d113a66cc481fdbae7e872e399ea2
Wim Lavrijsen <WLavrijsen@lbl.gov>
2025-10-23 15:04:21 -0700

    fix formatting

--- src/baqcs/models.py
+++ src/baqcs/models.py
@@ -22,25 +22,25 @@ def generic_ancilla_model_with_circuit(
     else:
         n_obs = 1
 
-    # Prior for ancilla (uniform or from circuit)
+  # Prior for ancilla (uniform or from circuit)
     ancilla_prior = torch.ones(2) / 2  # Uniform prior
 
     with pyro.plate("data", n_obs):
-        # Each observation has its own (latent) ancilla state
-        # The enumerate decorator will enumerate over this variable
+      # Each observation has its own (latent) ancilla state
+      # The enumerate decorator will enumerate over this variable
         ancilla = pyro.sample("ancilla", Categorical(ancilla_prior),
                             infer={"enumerate": "parallel"})
 
-        # Select the appropriate probabilities based on ancilla values
-        # When enumerated, ancilla will have an extra dimension
+      # Select the appropriate probabilities based on ancilla values
+      # When enumerated, ancilla will have an extra dimension
         if ancilla.dim() == 1:  # Not enumerated (shouldn't happen with decorator)
             system_probs = probability_table[ancilla.long()]
         else:  # Enumerated case - has extra enumeration dimension
-            # Ancilla shape: [2, n_obs] where 2 is enumeration dimension
-            # We need to gather the right probabilities
+          # Ancilla shape: [2, n_obs] where 2 is enumeration dimension
+          # We need to gather the right probabilities
             system_probs = probability_table[ancilla.long()]
 
-        # Observe system measurements
+      # Observe system measurements
         system = pyro.sample("system", Categorical(system_probs), obs=observations)
 
     return ancilla
@@ -57,20 +57,20 @@ def binomial_ancilla_model(observed_system_data, cpt):
     """
     n_samples = len(observed_system_data)
 
-    # Prior for the probability of ancilla being 1 (binomial parameter)
+  # Prior for the probability of ancilla being 1 (binomial parameter)
     p_ancilla_one = pyro.sample("p_ancilla_one", Beta(1, 1))
 
-    # For each observation
+  # For each observation
     with pyro.plate("data", n_samples):
         # Sample ancilla state from binomial distribution
         ancilla = pyro.sample("ancilla", Bernoulli(p_ancilla_one))
 
-        # Get the conditional probabilities for this ancilla state
-        # Convert to long tensor for indexing
+      # Get the conditional probabilities for this ancilla state
+      # Convert to long tensor for indexing
         ancilla_long = ancilla.long()
         probs = cpt[ancilla_long, :]
 
-        # Observe system measurement given ancilla state
+      # Observe system measurement given ancilla state
         pyro.sample("system", Categorical(probs),
                     obs=observed_system_data)
 
